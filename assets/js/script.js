0434b01295fc261ba420685496abf892be1f7eca
liebsen <overlemonsoft@gmail.com>
2021-02-13 20:58:30 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+	let contents = []
+	let pos = 0
+	let speed = document.getElementById('speed').value // secs
+	let clock = 0
+	let fx = document.getElementById('fx').value
+
+	document.getElementById('speed').addEventListener('change', e => {
+		e.preventDefault()
+		speed = e.target.value
+		cycle()
+	})
+
+	document.getElementById('fx').addEventListener('change', e => {
+		e.preventDefault()
+		fx = e.target.value
+	})
+
+	document.getElementById('pasteform').addEventListener('submit', e => {
+		e.preventDefault()
+		let text = document.getElementById('paste').value
+		contents = text.split(' ')
+		cycle()
+		return false
+	})
+
+	const cycle = () => {
+		if (clock) {
+			clearInterval(clock)
+		}
+		clock = setInterval(read, speed * 1000)
+	}
+
+	const read = () => {
+		const word = document.getElementById('word')
+		word.innerHTML = ''
+		const span = document.createElement('h1')
+		word.appendChild(span)
+		span.classList.add('animated', 'speed', fx)
+		if (pos <= contents.length) {
+			span.textContent = contents[pos]
+			pos++
+		} else {
+			span.textContent = ''
+			pos = 0
+		}
+		
+	}
+})
