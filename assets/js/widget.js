73ca4f58124d12dc30a6a50f2044909ac3d034ef
liebsen <overlemonsoft@gmail.com>
2021-12-30 20:51:10 -0300

    build

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -6,16 +6,22 @@ const e = (a, e, f) => {
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.speedread').forEach(e => {
-  	let contents = e.textContent.split(' ')
+  	let contents = e.textContent.trim().split(' ').filter(item => item && item.replace(/\n|\r/g, ""))
   	let pos = 0
     let clock = 0
   	let speed = e.getAttribute('speed') || 1
-    let words = e.getAttribute('words') || 1
+    let length = parseInt(e.getAttribute('length')) || 1
+    const ratio = Math.ceil(contents.length / length) + 1
+    const delta = contents.length % length
+
   	let fx = e.getAttribute('fx') || 'fadeIn'
     const progress = document.createElement('div')
     progress.classList.add('progress')
     e.parentNode.insertBefore(progress, e)
 
+    setTimeout(() => {
+      e.classList.add('animated', 'fadeIn')
+    }, 1000)
   	const cycle = () => {
   		if (clock) {
   			clearInterval(clock)
@@ -29,20 +35,16 @@ document.addEventListener('DOMContentLoaded', () => {
   		span.classList.add('animated', 'speed', fx)
   		e.innerHTML = ''
   		e.appendChild(span)
-
   		if (pos <= contents.length) {
-        for (var w = 0; w < parseInt(words); w++) {
-          p = pos + w + 1
-          if (contents[p - 1]) {
-            span.textContent+= contents[p - 1] + ' '
-          }
-        }
-        pos = p
+        span.textContent = contents.slice(pos, length + pos).join(' ')
+        pos+= length
+        const perc = parseFloat(pos / (contents.length + length - delta)) * 100
+        progress.style.width = perc + '%'
   		} else {
   			span.textContent = ''
+        progress.style.width = '0%'
   			pos = 0
-  		}		
-      progress.style.width = parseFloat(pos / contents.length) * 100 + '%'
+  		}      
   	}
     cycle()
   })
