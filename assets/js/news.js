e9ed35b38ce192e462884a5aac50a00889c9fb4e
liebsen <overlemonsoft@gmail.com>
2022-01-01 14:27:35 -0300

    build

--- assets/js/news.js
+++ assets/js/news.js
@@ -81,3 +81,24 @@ document.addEventListener('DOMContentLoaded', () => {
   })
   search()
 })
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+  e = e || window.event;
+
+  if (e.keyCode == '38') {
+    // up arrow
+  }
+  else if (e.keyCode == '40') {
+    // down arrow
+  }
+  else if (e.keyCode == '37') {
+    // left arrow
+    index-=2
+    cycle()
+  }
+  else if (e.keyCode == '39') {
+    cycle()
+  }
+}
