0c3548ddf4b42ff720af2c0096140177cbff738c
liebsen <overlemonsoft@gmail.com>
2022-01-01 20:51:01 -0300

    build

--- assets/js/news.js
+++ assets/js/news.js
@@ -5,6 +5,36 @@ let interval = 0
 const progress = document.querySelector('.progress')
 const speed = 15
 
+
+let playsound = (sound, vol) => {
+
+  if (sound === undefined) {
+    sound = 'move.mp3'
+  }
+
+  if (vol === undefined) {
+    vol = 0.5
+  }
+
+  const soundObj = new Audio(['/assets/sounds',sound].join('/'))
+  soundObj.volume = vol
+
+  if (sound) {
+    var playPromise = soundObj.play()
+    if (playPromise !== undefined) {
+      playPromise.then(_ => {
+        // Automatic playback started!
+        // Show playing UI.
+      }).catch(error => {
+        console.log(error)
+      // Auto-play was prevented
+      // Show paused UI.
+      })
+    }
+  }
+}
+
+
 let cycle = () => {
   progress.style.transitionDuration = speed + 's'
   if (pause) {
@@ -37,6 +67,8 @@ let search = () => {
   document.querySelector('.speedread').innerHTML = 'Fetching news...'
   document.querySelector('.speedread').classList.remove('fadeInRight', 'fadeOutLeft')
   document.querySelector('.speedread').classList.add('fadeIn')
+  document.querySelector('.updated').classList.remove('fadeIn', 'fadeOut')
+  document.querySelector('.updated').classList.add('fadeOut')
   index = 0
   progress.style.width = '0%'
   if (interval) {
@@ -55,16 +87,31 @@ let search = () => {
           pubDate: $(e).find("pubDate").text(),
           description: $(e).find("description").text()
         })
-      });
+      })
       if (!items.length) {
         document.querySelector('.speedread').innerHTML = 'No results for this search. Click search to try again.'
       } else {
+        playsound('updated.mp3')
+        updateTime()
         interval = setInterval(cycle, speed * 1000)
         cycle()
       }
     }
   })
   return false
+}
+
+let updateInt = 0
+let updateLast = 0
+let updateTime = () => {
+  updateLast = new Date()
+  document.querySelector('.updated').classList.add('fadeIn', 'delay3')
+  if (updateInt) {
+    clearInterval(updateInt)
+  }
+  updateInt = setInterval(() => {
+    document.querySelector('.updated-time').textContent = moment(updateLast).fromNow()
+  }, 1000 * 60)
 }
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.controls').classList.add('fadeInDown')
